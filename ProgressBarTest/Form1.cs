8c646ea2fcef99cf0c9975ea96639892f16d7e01
herbertagosto <herbertagosto@gmail.com>
2016-06-23 13:31:55 +0800

    initial commit

---
+++ ProgressBarTest/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgressBarTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // start
+            ProgressBar.UI.Start(this);
+            for (int i = 0; i < 2; i++)
+            {
+                // long process..
+                System.Threading.Thread.Sleep(1000);
+            }
+            // stop/exit
+            ProgressBar.UI.Stop();
+        }
+    }
+}
